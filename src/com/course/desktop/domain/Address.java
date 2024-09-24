d649b711dfa7a14d1636603d834aca20ace0b5f5
shaineDoc <shainanin@yandex.ru>
2024-09-24 21:30:55 +0400

    изменение логики обращения к улицам

--- src/com/course/desktop/domain/Address.java
+++ src/com/course/desktop/domain/Address.java
@@ -1,13 +1,13 @@
 package com.course.desktop.domain;
 
 public class Address {
-    private String street;
+    private Street street;
     private String building;
     private String extension;
     private String postCode;
     private String apartment;
 
-    public Address(String postCode, String street, String building, String extension, String apartment) {
+    public Address(String postCode, Street street, String building, String extension, String apartment) {
         this.postCode = postCode;
         this.street = street;
         this.building = building;
@@ -23,11 +23,11 @@ public class Address {
         this.postCode = postCode;
     }
 
-    public String getStreet() {
+    public Street getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
+    public void setStreet(Street street) {
         this.street = street;
     }
 
