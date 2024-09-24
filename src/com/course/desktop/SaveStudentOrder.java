3d8a3012dd716a58941f150848b0d5c6e4da5d63
shaineDoc <shainanin@yandex.ru>
2024-09-24 21:32:12 +0400

    Изменение логики обращения к адресу

--- src/com/course/desktop/SaveStudentOrder.java
+++ src/com/course/desktop/SaveStudentOrder.java
@@ -1,9 +1,6 @@
 package com.course.desktop;
 
-import com.course.desktop.domain.Address;
-import com.course.desktop.domain.Adult;
-import com.course.desktop.domain.Child;
-import com.course.desktop.domain.StudentOrder;
+import com.course.desktop.domain.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,14 +11,7 @@ import java.time.LocalDate;
 public class SaveStudentOrder
 {
     public static void main(String[] args) throws Exception{
-        Class.forName("org.postgresql.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/",
-                "postgres","admin");
-        Statement stmt = connection.createStatement();
-        ResultSet rs =  stmt.executeQuery("SELECT * FROM jc_street");
-        while (rs.next()) {
-            System.out.println(rs.getLong(1) + ": " + rs.getString(2));
-        }
+
         StudentOrder s = buildStudentOrder(10);
 //        StudentOrder so = new StudentOrder();
 //        long ans = saveStudentOrder(so);
@@ -42,7 +32,9 @@ public class SaveStudentOrder
         so.setMarriageDate(LocalDate.of(2016, 7, 4));
         so.setMarriageOffice("Отдел ЗАГС");
 
-        Address address = new Address("195000", "Заневский пр.", "12", "", "142");
+        Street street = new Street(1L,"First street");
+
+        Address address = new Address("195000", street, "12", "", "142");
 
         // Муж
         Adult husband = new Adult("Петров", "Виктор", "Сергеевич", LocalDate.of(1997, 8, 24));
@@ -79,6 +71,5 @@ public class SaveStudentOrder
         so.addChild(child2);
 
         return so;
-        // ТЕСТ ВЫГРУЗКИ НА ГИТ
     }
 }
